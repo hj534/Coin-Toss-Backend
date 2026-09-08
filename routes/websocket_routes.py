155ef0c8631ff85094c144d5fc6781a3ca842a1a
hj534 <hj534221@gmail.com>
2026-09-08 13:15:08 +0500

    add per-type tournament list routes update1

--- routes/websocket_routes.py
+++ routes/websocket_routes.py
@@ -11,19 +11,3 @@ async def websocket_endpoint(websocket: WebSocket, playfab_id: str):
     except WebSocketDisconnect:
         manager.disconnect(playfab_id)
         
-
-@router.get("/get_free_tournaments/", response_model=list[TournamentOut])
-async def get_free_tournaments():
-    return await service.get_free_tournaments()
-
-@router.get("/get_daily_tournaments/", response_model=list[TournamentOut])
-async def get_daily_tournaments():
-    return await service.get_daily_tournaments()
-
-@router.get("/get_weekly_tournaments/", response_model=list[TournamentOut])
-async def get_weekly_tournaments():
-    return await service.get_weekly_tournaments()
-
-@router.get("/get_monthly_tournaments/", response_model=list[TournamentOut])
-async def get_monthly_tournaments():
-    return await service.get_monthly_tournaments()
